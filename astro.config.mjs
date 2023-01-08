a67a6c46bf6fa7f681ad850ee01964188e0d2bb8
Ihar Trafimovich <unutranyholas@gmail.com>
2023-01-08 20:41:12 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "middleware"
+  })
+});
